e053f1c114d75f8ffc6b9ef67d07ddfeb8941cce
karanpo <karolina_ania@o2.pl>
2016-10-23 15:27:11 +0200

    camera: zoom + moving and swaping model

---
+++ FirstStagePrototype/Assets/Scripts/SwapModel.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapModel : MonoBehaviour
+{
+
+    public GameObject tree;
+    public GameObject newModel;
+
+
+
+    private bool mSwapModel = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mSwapModel && tree != null)
+        {
+            SwapingModel();
+            mSwapModel = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(50, 50, 120, 40), "Swap Model"))
+        {
+            mSwapModel = true;
+            
+        }
+    }
+
+    private void SwapingModel()
+    {
+
+        GameObject trackableGameObject = tree;
+        Vector3 pos = new Vector3(0,0,0);
+        //disable any pre-existing augmentation
+        for (int i = 0; i < trackableGameObject.transform.childCount; i++)
+        {
+            Transform child = trackableGameObject.transform.GetChild(i);
+            pos = child.transform.position;
+            child.gameObject.SetActive(false);
+            
+        }
+
+       
+        GameObject bigTree = (GameObject)Instantiate(newModel, pos, Quaternion.Euler(0, 0, 0));
+
+        // Re-parent the cube as child of the trackable gameObject
+        bigTree.transform.parent = tree.transform;
+
+        
+        // Make sure it is active
+        bigTree.SetActive(true);
+    }
+}
